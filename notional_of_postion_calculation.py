417178730712992d42462855f7eb2dd7cfdecef2
yumingwjobjob <yumingwang212@gmail.com>
2021-07-14 01:01:43 -0500

    add the error handling and singleton method

--- notional_of_postion_calculation.py
+++ notional_of_postion_calculation.py
@@ -1,7 +1,9 @@
+from Yuming_Wang_Yahoo_finance.yahoo_finance_api_client import yahoo_finance_api
 import yahoo_finance_api_client
 class notion_of_position_calculation:
         def get_notional_of_position(self,quantity, symbol):
-            yahoo_finance_api = yahoo_finance_api_client.yahoo_finance_api()
+            yahoo_finance_api = yahoo_finance_api_client.get_instance()
+            #yahoo_finance_api = yahoo_finance_api_client.yahoo_finance_api()
             raw_price = yahoo_finance_api.get_regular_market_previous_close_symbol(symbol)
             quantity = float(quantity)
             final_price = raw_price * quantity
--- yahoo_finance_api_client.py
+++ yahoo_finance_api_client.py
@@ -1,19 +1,42 @@
 import requests
 import json
 class yahoo_finance_api:
-    def get_regular_market_previous_close_symbol(self,symbol):
-        url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary"
 
-        querystring = {"symbol":symbol,"region":"US"}
+    #set a singleton method
+    __instance = None
+    def get_instance():
 
-        headers = {
+        if yahoo_finance_api.__instance == None:
+            yahoo_finance_api()
+            return yahoo_finance_api.__instance
+    
+    def __init__(self):
+        if yahoo_finance_api.__instance != None:
+            print("ERROR")
+        else:
+            yahoo_finance_api.__instance = self
+    #set url and headers tp static
+    url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary"
+    headers = {
         'x-rapidapi-key': "1a5d467378msh93fb379ab511567p1a1a82jsn0f1bb63d2039",
         'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
             }
-        response = requests.request("GET", url, headers=headers, params=querystring)
+    def get_regular_market_previous_close_symbol(self,symbol):
+
+        querystring = {"symbol":symbol,"region":"US"}
+        response = requests.request("GET", self.url, headers=self.headers, params=querystring)
         payload = json.loads(response.text)
+
+        
         #(response.text)
         #print(payload["price"])
         #print(payload)
-        raw_price = payload["price"]['regularMarketPreviousClose']['raw']
+        #add a error handling
+        while True:
+            try:
+                raw_price = payload["price"]['regularMarketPreviousClose']['raw']
+                break
+            except(RuntimeError, TypeError, NameError):
+                print("ERROR, EMPTY PRICE")
+                return 0
         return float(raw_price)
